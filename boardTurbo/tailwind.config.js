c277932c47259e9b8ea4116ca97ee60e95cd3083
raul <74162376+raulanto@users.noreply.github.com>
2024-01-10 20:55:46 -0600

    confg tailwin

---
+++ boardTurbo/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}', 'index.html',    "./index.html",],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
