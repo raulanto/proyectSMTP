f0f11f8c50dacdd5dd961d5c45a0d95dc5939368
raul <74162376+raulanto@users.noreply.github.com>
2024-01-13 19:51:42 -0600

    cambios de flobite

--- boardTurbo/tailwind.config.js
+++ boardTurbo/tailwind.config.js
@@ -1,8 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}', 'index.html',    "./index.html",],
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}', 'index.html', './index.html'],
+  darkMode: 'class',
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {"50":"#fefce8","100":"#fef9c3","200":"#fef08a","300":"#fde047","400":"#facc15","500":"#eab308","600":"#ca8a04","700":"#a16207","800":"#854d0e","900":"#713f12","950":"#422006"}
+      }
+    },
+    fontFamily: {
+      'body': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ],
+      'sans': [
+    'Inter', 
+    'ui-sans-serif', 
+    'system-ui', 
+    '-apple-system', 
+    'system-ui', 
+    'Segoe UI', 
+    'Roboto', 
+    'Helvetica Neue', 
+    'Arial', 
+    'Noto Sans', 
+    'sans-serif', 
+    'Apple Color Emoji', 
+    'Segoe UI Emoji', 
+    'Segoe UI Symbol', 
+    'Noto Color Emoji'
+  ]
+    }
   },
-  plugins: []
+  plugins: [require('flowbite/plugin')],
 }
